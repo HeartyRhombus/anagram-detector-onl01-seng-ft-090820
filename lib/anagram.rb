68b442251a3833f9b5ca05bc80509940ae9aa4ee
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-18 20:51:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
 
   def match(array)
     array.each do |word|
-      if word.split("").sort == array.sort
+      if word.split("").sort == array.split("").sort
         word
       end
     end
