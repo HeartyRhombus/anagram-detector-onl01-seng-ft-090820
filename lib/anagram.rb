540ac2cc0d72e6fc002200375d133e3279f03727
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-18 21:38:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,9 @@ class Anagram
     word = @word.split("").sort.join
     sorted_letters = array.collect do |word|
       letters = word.split("").sort.join
-
+      if word.length == letters.length
+        word.all?(letters)
+        letters
     end
 
 
