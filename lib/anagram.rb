1929887fc280c1d269d20511c211ce07f6e5e817
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-18 21:48:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,14 +9,19 @@ class Anagram
   end
 
   def match(array)
-    word = @word.split("").sort.join
-    sorted_letters = array.collect do |word|
-      letters = word.split("").sort.join
-      if letters.include?(word)
-        @word
-      else
-        []
+    # word = @word.split("").sort.join
+    # sorted_letters = array.collect do |word|
+    #   letters = word.split("").sort.join
+    #   if letters.include?(word)
+    #     @word
+    #   else
+    #     []
+    #   end
+      sorted_words = array.each_with_object(Hash.new []) do |letters, word|
+        word[letters.chars.sort] += [letters]
       end
+      sorted_words.each do |letters, matching_word|
+        matching_word.join(", ")
     end
 
 
